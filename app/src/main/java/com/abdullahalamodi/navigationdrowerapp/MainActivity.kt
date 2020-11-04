17e92fa9c583c6b1bf5d654dbb9e7d326595063a
abdullahalamodi <39647459+abdullahalamodi@users.noreply.github.com>
2020-11-05 00:31:45 +0300

    init commit

---
+++ app/src/main/java/com/abdullahalamodi/navigationdrowerapp/MainActivity.kt
@@ -0,0 +1,195 @@
+package com.abdullahalamodi.navigationdrowerapp
+
+import ClickListener
+import RecyclerTouchListener
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var activity_main_toolbar: Toolbar;
+    private lateinit var adapter: NavigationRVAdapter
+
+    private var items = arrayListOf(
+        NavigationItemModel(R.drawable.ic_home, "Home"),
+        NavigationItemModel(R.drawable.ic_music, "Music"),
+        NavigationItemModel(R.drawable.ic_movie, "Movies"),
+        NavigationItemModel(R.drawable.ic_book, "Books"),
+        NavigationItemModel(R.drawable.ic_profile, "Profile"),
+        NavigationItemModel(R.drawable.ic_settings, "Settings"),
+        NavigationItemModel(R.drawable.ic_social, "Like us on facebook")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        activity_main_toolbar = findViewById(R.id.toolbar)
+
+        // Set the toolbar
+        setSupportActionBar(activity_main_toolbar)
+
+
+        // Setup Recyclerview's Layout
+        navigation_rv.layoutManager = LinearLayoutManager(this)
+        navigation_rv.setHasFixedSize(true)
+
+
+        // Update Adapter with item data and highlight the default menu item ('Home' Fragment)
+        updateAdapter(0);
+
+        // Set 'Home' as the default fragment when the app starts
+        val homeFragment = DemoFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.activity_main_content_id, homeFragment).commit()
+        updateActionBarTitle("homeFragment");
+
+        // Add Item Touch Listener
+        navigation_rv.addOnItemTouchListener(RecyclerTouchListener(this, object : ClickListener {
+            override fun onClick(view: View, position: Int) {
+                when (position) {
+                    0 -> {
+                        // # Home Fragment
+                        val homeFragment = DemoFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.activity_main_content_id, homeFragment).commit()
+                        updateActionBarTitle("homeFragment");
+                    }
+                    1 -> {
+                        // # Music Fragment
+                        val musicFragment = DemoFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.activity_main_content_id, musicFragment).commit()
+                        updateActionBarTitle("musicFragment");
+                    }
+                    2 -> {
+                        // # Movies Fragment
+                        val moviesFragment = DemoFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.activity_main_content_id, moviesFragment).commit()
+                        updateActionBarTitle("moviesFragment")
+                    }
+                    3 -> {
+                        // # Books Fragment
+                        val booksFragment = DemoFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.activity_main_content_id, booksFragment).commit()
+                        updateActionBarTitle("booksFragment")
+                    }
+                    4 -> {
+                        // # Profile Activity
+//                        val intent = Intent(this@MainActivity, DemoActivity::class.java)
+//                        startActivity(intent)
+                    }
+                    5 -> {
+                        // # Settings Fragment
+                        val settingsFragment = DemoFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.activity_main_content_id, settingsFragment).commit()
+                        updateActionBarTitle("settingsFragment");
+                    }
+                    6 -> {
+                        // # Open URL in browser
+//                        val uri: Uri = Uri.parse("https://johnc.co/fb")
+//                        val intent = Intent(Intent.ACTION_VIEW, uri)
+//                        startActivity(intent)
+                    }
+                }
+                // Don't highlight the 'Profile' and 'Like us on Facebook' item row
+                if (position != 6 && position != 4) {
+                    updateAdapter(position)
+                }
+                //close the drawer
+                Handler().postDelayed({
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }, 200)
+            }
+        }));
+
+        // Close the soft keyboard when you open or close the Drawer
+        val toggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            activity_main_toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        ) {
+            override fun onDrawerClosed(drawerView: View) {
+                // Triggered once the drawer closes
+                super.onDrawerClosed(drawerView)
+                try {
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+                } catch (e: Exception) {
+                    e.stackTrace
+                }
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                // Triggered once the drawer opens
+                super.onDrawerOpened(drawerView)
+                try {
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+                } catch (e: Exception) {
+                    e.stackTrace
+                }
+            }
+        }
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+
+
+        // Set Header Image
+        navigation_header_img.setImageResource(R.drawable.logo)
+
+        // Set background of Drawer
+        navigation_layout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+
+
+    }
+
+    private fun updateAdapter(highlightItemPos: Int) {
+        adapter = NavigationRVAdapter(items, highlightItemPos)
+        navigation_rv.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun updateActionBarTitle(title: String) {
+        supportActionBar?.title = title;
+    }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            // Checking for fragment count on back stack
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                // Go to the previous fragment
+                supportFragmentManager.popBackStack()
+            } else {
+                // Exit the app
+                super.onBackPressed()
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/abdullahalamodi/navigationdrowerapp/NavigationItemModel.kt
@@ -0,0 +1,3 @@
+package com.abdullahalamodi.navigationdrowerapp
+
+data class NavigationItemModel(var icon: Int, var title: String)
